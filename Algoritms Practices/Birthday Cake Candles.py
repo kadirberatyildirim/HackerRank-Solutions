82ca60fae47e1f13c5374cbcf66b8c8eb6fb9dd4
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2021-11-28 23:39:55 +0300

    day 11 and 2 more algorithms

---
+++ Algoritms Practices/Birthday Cake Candles.py
@@ -0,0 +1,40 @@
+"""
+You are in charge of the cake for a child's birthday. 
+You have decided the cake will have one candle for each year of their total age. 
+They will only be able to blow out the tallest of the candles. 
+Count how many candles are tallest. 
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    max_candle = max(candles)
+    
+    return len( [i for i in candles if i == max_candle] )
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
