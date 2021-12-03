136030daa437ccb0f9b4822b842d2fd319024ea2
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2021-12-03 20:24:37 +0300

    3 more python practices

---
+++ Python Practices/Math - Triangle Quest.py
@@ -0,0 +1,15 @@
+"""
+You are given a positive integer N. Print a numerical triangle of height N-1 like the one below:
+
+1
+22
+333
+4444
+55555
+......
+
+Can you do it using only arithmetic operations, a single for loop and print statement? 
+"""
+
+for i in range(1, int(input())): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(int(i * 10**i / 9))
