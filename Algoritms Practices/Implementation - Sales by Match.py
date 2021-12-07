b824da7cf73d95f243ca910b0ed6eb4598b0ac9c
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2021-12-07 17:09:36 +0300

    1 more algorithm practice

---
+++ Algoritms Practices/Implementation - Sales by Match.py
@@ -0,0 +1,42 @@
+"""
+There is a large pile of socks that must be paired by color. 
+Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    pairs = 0
+    for element in set(ar):
+        pairs += len( [i for i in ar if i == element] ) // 2
+        
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
