7ef14094bb3d46ee35bb4de0336d93cd90c8ed4d
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2021-12-11 23:05:53 +0300

    Day 23 of Coding

---
+++ 30 Days of Code/Day 23 BST Level-Order Traversal.py
@@ -0,0 +1,46 @@
+"""
+A level-order traversal, also known as a breadth-first search, visits each level of a tree's nodes from left to right, top to bottom. 
+You are given a pointer, root, pointing to the root of a binary search tree. 
+Complete the levelOrder function provided in your editor so that it prints the level-order traversal of the binary search tree.
+
+Hint: You'll find a queue helpful in completing this challenge. 
+"""
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def levelOrder(self,root):
+        #Write your code here
+        if root is None: return 
+    
+        queue = [root]
+
+        while len(queue) !=0:
+            tmp = queue.pop(0)
+            print(tmp.data, end=' ')
+            if tmp.left is not None: queue.append(tmp.left)
+            if tmp.right is not None: queue.append(tmp.right)
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
