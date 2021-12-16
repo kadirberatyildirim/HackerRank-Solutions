d939b1554e6bf86b85913f9341c2ff3462868fde
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2021-12-16 23:45:12 +0300

    Update Apple and Orange.py

--- Algoritms Practices/Apple and Orange.py
+++ Algoritms Practices/Apple and Orange.py
@@ -9,7 +9,7 @@ In the diagram below:
 
     Assume the trees are located on a single point, where the apple tree is at point a, and the orange tree is at point b.
     When a fruit falls from its tree, it lands d units of distance from its tree of origin along the x-axis. 
-    *A negative value of d means the fruit fell d units to the tree's left, and a positive value of d means it falls d units to the tree's right.*
+    A negative value of d means the fruit fell d units to the tree's left, and a positive value of d means it falls d units to the tree's right.
 
 Given the value of d for m apples and n oranges, determine how many apples and oranges will fall on Sam's house (i.e., in the inclusive range [s,t])?
 """
