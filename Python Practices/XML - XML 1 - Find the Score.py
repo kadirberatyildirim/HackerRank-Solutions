9449bc367ff5d820afc791d54af28614551a2807
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2021-12-27 16:31:18 +0300

    Create XML - XML 1 - Find the Score.py

---
+++ Python Practices/XML - XML 1 - Find the Score.py
@@ -0,0 +1,21 @@
+"""
+You are given a valid XML document, and you have to print its score. 
+The score is calculated by the sum of the score of each element. 
+For any element, the score is equal to the number of attributes it has. 
+"""
+
+import sys
+import xml.etree.ElementTree as etree
+
+def get_attr_number(node):
+    # your code goes here
+    count = sum([get_attr_number(tag) for tag in node])
+    
+    return count + len(node.attrib)
+
+if __name__ == '__main__':
+    sys.stdin.readline()
+    xml = sys.stdin.read()
+    tree = etree.ElementTree(etree.fromstring(xml))
+    root = tree.getroot()
+    print(get_attr_number(root))
