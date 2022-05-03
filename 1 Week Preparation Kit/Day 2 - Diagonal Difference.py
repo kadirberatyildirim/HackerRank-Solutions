602c18ff7f0a15f2c78d13479bc3bcf67872566f
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-05-03 23:35:48 +0300

    Update Day 2 - Diagonal Difference.py

--- 1 Week Preparation Kit/Day 2 - Diagonal Difference.py
+++ 1 Week Preparation Kit/Day 2 - Diagonal Difference.py
@@ -6,7 +6,7 @@ For example, the square matrix arr is shown below:
 4 5 6
 9 8 9  
 
-The left-to-right diagonal = 1+5+9 = 15. The right to left diagonal = 3+5+9 = 17. Their absolute difference is |15 - 17| = 2.
+The left-to-right diagonal = 1+5+9 = 15. The right to left diagonal = 3+5+9 = 17. Their absolute difference is |15 - 17| = 2
 """
 
 #!/bin/python3
