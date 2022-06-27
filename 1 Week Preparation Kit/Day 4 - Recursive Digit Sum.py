e4d28c0159b8c8a82ea15064699aeedf839c9dad
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-06-27 17:36:57 +0300

    Update Day 4 - Recursive Digit Sum.py

--- 1 Week Preparation Kit/Day 4 - Recursive Digit Sum.py
+++ 1 Week Preparation Kit/Day 4 - Recursive Digit Sum.py
@@ -4,7 +4,7 @@ We define super digit of an integer x using the following rules:
 Given an integer, we need to find the super digit of the integer.
 
     If x has only digit, then its super digit is x.
-    Otherwise, the super digit of x is equal to the super digit of the sum of the digits of x.
+    Otherwise, the super digit of x is equal to the super digit of the sum of the digits of x
 """
 
 #!/bin/python3
