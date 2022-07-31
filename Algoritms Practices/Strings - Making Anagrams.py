f75ab11d3bbb536ea8336425fd44c463109f46c6
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-07-31 13:43:09 +0300

    Create Strings - Making Anagrams.py

---
+++ Algoritms Practices/Strings - Making Anagrams.py
@@ -0,0 +1,47 @@
+"""
+We consider two strings to be anagrams of each other if the first string's letters can be rearranged to form the second string. 
+In other words, both strings must contain the same exact letters in the same exact frequency. 
+For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+
+Alice is taking a cryptography class and finding anagrams to be very useful. 
+She decides on an encryption scheme involving two large strings where encryption is dependent 
+on the minimum number of character deletions required to make the two strings anagrams. 
+Can you help her find this number? 
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'makingAnagrams' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. STRING s1
+#  2. STRING s2
+#
+
+def makingAnagrams(s1, s2):
+    # Write your code here
+    from collections import Counter
+    counts = Counter(s1)
+    counts.subtract(s2)
+    return sum(abs(x) for x in counts.values())
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s1 = input()
+
+    s2 = input()
+
+    result = makingAnagrams(s1, s2)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
