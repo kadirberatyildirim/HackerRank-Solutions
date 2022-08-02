03731d610e907cb7e790e12914b5dd2138663072
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-08-02 22:30:06 +0300

    Create Implementation - Equalize the Array.py

---
+++ Algoritms Practices/Implementation - Equalize the Array.py
@@ -0,0 +1,37 @@
+"""
+Given an array of integers, determine the minimum number of elements to delete to leave only elements of equal value.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'equalizeArray' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def equalizeArray(arr):
+    # Write your code here
+    from collections import Counter
+    
+    return len(arr) - max(Counter(arr).values())
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = equalizeArray(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
