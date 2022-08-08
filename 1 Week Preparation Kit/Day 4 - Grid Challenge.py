1ed7ac42f118b086ea3d7c40a50d0fadb4a3e053
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-08-08 16:20:19 +0300

    Create Day 4 - Grid Challenge.py

---
+++ 1 Week Preparation Kit/Day 4 - Grid Challenge.py
@@ -0,0 +1,46 @@
+"""
+Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending. 
+Determine if the columns are also in ascending alphabetical order, top to bottom. Return YES if they are or NO if they are not.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gridChallenge' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING_ARRAY grid as parameter.
+#
+
+def gridChallenge(grid):
+    # Write your code here
+    row = [sorted(g) for g in grid]
+    col = [list(x) for x in zip(*row)]
+    test = [sorted(t) for t in col]
+    return 'YES' if row == sorted(row) and col == test else 'NO'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
