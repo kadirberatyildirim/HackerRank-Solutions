8c12a3bd6bad2950b174412819d0112c2976d055
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2022-08-10 22:08:33 +0300

    Update Day 4 - Grid Challenge.py

--- 1 Week Preparation Kit/Day 4 - Grid Challenge.py
+++ 1 Week Preparation Kit/Day 4 - Grid Challenge.py
@@ -1,6 +1,6 @@
 """
 Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending. 
-Determine if the columns are also in ascending alphabetical order, top to bottom. Return YES if they are or NO if they are not.
+Determine if the columns are also in ascending alphabetical order, top to bottom. Return YES if they are or NO if they are not
 """
 
 #!/bin/python3
