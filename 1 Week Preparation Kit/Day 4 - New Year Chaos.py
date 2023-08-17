fe742c04d5bc139ada259c8e58370b933630ca68
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2023-08-17 12:05:04 +0300

    Create Day 4 - New Year Chaos.py

---
+++ 1 Week Preparation Kit/Day 4 - New Year Chaos.py
@@ -0,0 +1,48 @@
+"""
+It is New Year's Day and people are in line for the Wonderland rollercoaster ride. 
+Each person wears a sticker indicating their initial position in the queue from 1 to n. 
+Any person can bribe the person directly in front of them to swap positions, but they still wear their original sticker. 
+One person can bribe at most two others.
+
+Determine the minimum number of bribes that took place to get to a given queue order. 
+Print the number of bribes, or, if anyone has bribed more than two people, print Too chaotic.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'minimumBribes' function below.
+#
+# The function accepts INTEGER_ARRAY q as parameter.
+#
+
+
+def minimumBribes(q):
+    moves = 0
+    for i in range(len(q)):
+        if q[i] - (i + 1) > 2:
+            print("Too chaotic")
+            return
+
+        for j in range(max(0, q[i] - 2), i):
+            if q[j] > q[i]:
+                moves += 1
+
+    print(moves)
+
+
+if __name__ == "__main__":
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        q = list(map(int, input().rstrip().split()))
+
+        minimumBribes(q)
