723c1b0a8b46ab45bd11d8ed14ecf75dbdd60dd0
Kadir Berat YILDIRIM <kadirberatyildirim@protonmail.com>
2023-08-19 16:33:07 +0300

    Create Regex and Parsing - Detect HTML Tags, Attributes and Attribute Values.py

---
+++ Python Practices/Regex and Parsing - Detect HTML Tags, Attributes and Attribute Values.py
@@ -0,0 +1,44 @@
+"""
+You are given an HTML code snippet of N lines.
+Your task is to detect and print all the HTML tags, attributes and attribute values.
+
+Print the detected items in the following format:
+
+Tag1
+Tag2
+-> Attribute2[0] > Attribute_value2[0]
+-> Attribute2[1] > Attribute_value2[1]
+-> Attribute2[2] > Attribute_value2[2]
+Tag3
+-> Attribute3[0] > Attribute_value3[0]
+
+
+The -> symbol indicates that the tag contains an attribute. It is immediately followed by the name of the attribute and the attribute value.
+The > symbol acts as a separator of attributes and attribute values.
+
+If an HTML tag has no attribute then simply print the name of the tag.
+
+Note: Do not detect any HTML tag, attribute or attribute value inside the HTML comment tags (<!-- Comments -->). Comments can be multiline.
+All attributes have an attribute value.
+"""
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        for attr in attrs:
+            print(f"-> {attr[0]} > {attr[1]}")
+    
+    def handle_startendtag(self, tag, attrs):
+        print(tag)
+        for attr in attrs:
+            print(f"-> {attr[0]} > {attr[1]}")
+
+n = int(input())
+html_lines = [input() for _ in range(n)]
+
+parser = MyHTMLParser()
+parser.feed('\n'.join(html_lines))
